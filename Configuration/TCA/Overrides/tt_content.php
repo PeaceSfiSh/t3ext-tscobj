56511c1af73924fe1ccd2a08d71086e995a93bb7
Thomas Scholze <thomas.scholze@gmail.com>
2020-07-09 11:39:25 +0200

    [TASK] move TCA related methods to TCA/Overrides

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,4 +1,21 @@
 <?php
+defined('TYPO3_MODE') or die();
+
+// Add plugins
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
+    [
+        'LLL:EXT:tscobj/Resources/Private/Language/locallang_db.xlf:tt_content.list_type_pi1',
+        'tscobj_pi1'
+    ],
+    'list_type',
+    'tscobj'
+);
+
+// Add flexform DataStructures
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'tscobj_pi1',
+    'FILE:EXT:tscobj/Configuration/FlexForms/flexform_ds_pi1.xml'
+);
 
 // Plugin options
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['tscobj_pi1'] = 'layout,select_key,pages,recursive';
